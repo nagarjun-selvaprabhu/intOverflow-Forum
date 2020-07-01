5ef19005d94426d974f23f70f62fb7d5365a1abd
nagarjun <nagarjun000333@gmail.com>
2020-07-01 22:55:20 +0530

    spring boot zero

---
+++ intOverflow/src/main/java/com/rootQuotient/intOverflow/IntOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.rootQuotient.intOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntOverflowApplication.class, args);
+	}
+
+}
---
+++ intOverflow/src/test/java/com/rootQuotient/intOverflow/IntOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootQuotient.intOverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
